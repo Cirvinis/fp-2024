d86f4e8c969674a60c7e4c1a77d5eded66b158d0
Titas Vaitkus <titas.vaitkus@mif.stud.vu.lt>
2024-09-15 17:54:56 +0300

    Pakeistas Lib1.hs failas, papildytas completions list, kad praeitu testa ir sukurtas naujas grammar.txt failas kuriam yra BNF grammar

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,14 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = 
+    [
+    "Directory: \"Root\""
+    , "Directory: \"Documents\""
+    , "Directory: \"Photos\""
+    , "File: \"README.txt\" Size: 123"
+    , "File: \"Resume.docx\" Size: 456"
+    , "File: \"Vacation.jpg\" Size: 789"
+    , "File: \"NewYear.png\" Size: 1011"
+    , "Directory: \"2024\""
+    ]
